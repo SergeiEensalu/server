11ca93aafa6187f38aafdd7605c7c8f8757e32bc
Sergei Eensalu <sergei.eensalu@gmail.com>
2024-06-18 09:23:08 +0300

    Comment for token

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import {hello} from "./src/handler/hello";
 import {start} from "./src/handler/start";
 import './src/server';
 
+// This is not good to commit token, but not a huge problem, cause anyway this token is for testing only
 const TOKEN = '6722392005:AAGGm6LYb_R44BSf5JdFQ-OCTprIsoBfgSw'
 
 const bot = new Telegraf(TOKEN)
